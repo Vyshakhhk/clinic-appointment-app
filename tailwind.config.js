b65eaa1ed1ff206153b4d1fb0af821ce99f60d4d
Vyshakhhk <vyshakhh47@gmail.com>
2025-07-13 16:37:30 +0530

    Changed the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-import scrollbarHide from 'tailwind-scrollbar-hide'
-
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class', //  Enable dark mode via class
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
   },
-  plugins: [
-    scrollbarHide, // Import and use scrollbar-hide plugin
-  ],
-}
+  plugins: [require("tailwind-scrollbar-hide")],
+};
